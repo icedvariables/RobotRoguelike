636517041e7f33959a4fddda6a4f153cc8f931af
icedvariables <macodeblacko@gmail.com>
2017-07-01 11:00:35 +0100

    Removed unused imports.

--- src/robotroguelike/items/ItemIronIngot.java
+++ src/robotroguelike/items/ItemIronIngot.java
@@ -1,7 +1,5 @@
 package robotroguelike.items;
 
-import robotroguelike.game.Game;
-
 public class ItemIronIngot extends Item {
 	public ItemIronIngot(){
 		super(2, "Iron Ingot", "A lump of pure iron.", Tier.MEDIUM, false);
--- src/robotroguelike/items/ItemTest.java
+++ src/robotroguelike/items/ItemTest.java
@@ -1,7 +1,5 @@
 package robotroguelike.items;
 
-import robotroguelike.game.Game;
-
 public class ItemTest extends Item {
 	public ItemTest(){
 		super(100, "Test Item", "You shouldn't have this...", Tier.HIGH, false);
